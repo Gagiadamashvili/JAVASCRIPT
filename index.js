f6e088c512c23ccb71556c63b789d4cab1c5c0db
Gagi Adamashvili <gagiadamashvili@gmail.com>
2023-06-25 03:34:52 +0400

    add project

---
+++ index.js
@@ -0,0 +1,22 @@
+
+// return the sum of all the positives ones 
+
+// let arr = [1, -2, 3, -4, 5, -10, -25]
+
+// positiveNumber = arr.filter(arr => arr > 0)
+
+// console.log(positiveNumber)
+
+
+// Given an array of strings, remove the last character of each string
+
+
+// let arr = ["kita", "alexandre", "vache", "sandro"];
+
+// for(let i = 0; i < arr.length; i++){
+//     arr[i] = arr[i].slice(0, -1);
+// }
+
+// console.log(arr)
+
+
